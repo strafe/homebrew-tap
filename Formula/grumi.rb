2d4560b7cb6a5e73bfb91cad588d97d3b2dd876e
strafe <strafe@users.noreply.github.com>
2019-05-07 18:11:22 +0100

    Add grumi

---
+++ Formula/grumi.rb
@@ -0,0 +1,10 @@
+class Grumi < Formula
+  desc 'Upload an image, video or album to Imgur'
+  homepage 'https://github.com/strafe/grumi'
+  url 'https://github.com/strafe/grumi.git'
+  version '0.0.1'
+
+  def install
+    bin.install 'grumi'
+  end
+end
