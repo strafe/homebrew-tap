67c5a930617e9d2771436bf1b4e1e9ad61c40b7a
strafe <strafe@users.noreply.github.com>
2019-05-16 11:06:54 +0100

    Update grumi to 0.0.4

--- Formula/grumi.rb
+++ Formula/grumi.rb
@@ -2,7 +2,7 @@ class Grumi < Formula
   desc 'Upload an image, video or album to Imgur'
   homepage 'https://github.com/strafe/grumi'
   url 'https://github.com/strafe/grumi.git'
-  version '0.0.1'
+  version '0.0.4'
 
   def install
     bin.install 'grumi'
