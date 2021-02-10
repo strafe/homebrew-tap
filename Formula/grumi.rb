dbecf40ee13f29e1054954e22de5f580303a436d
strafe <strafe@users.noreply.github.com>
2021-02-10 23:15:15 +0000

    Add jq as a dependency from grumi

--- Formula/grumi.rb
+++ Formula/grumi.rb
@@ -1,10 +1,13 @@
 class Grumi < Formula
-  desc 'Upload an image, video or album to Imgur'
-  homepage 'https://github.com/strafe/grumi'
-  url 'https://github.com/strafe/grumi.git'
-  version '0.2.0'
+  desc "Upload an image, video or album to Imgur"
+  homepage "https://github.com/strafe/grumi"
+  url "https://github.com/strafe/grumi.git"
+  version "0.2.0"
+  license "MIT"
+  
+  depends_on "jq"
 
   def install
-    bin.install 'grumi'
+    bin.install "grumi"
   end
 end
