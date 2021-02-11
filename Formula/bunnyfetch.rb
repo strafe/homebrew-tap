e2cab0c0ee15baec112a2c6f39b09c1c5c009722
strafe <strafe@users.noreply.github.com>
2021-02-11 11:59:10 +0000

    Update bunnyfetch to 0.0.2

--- Formula/bunnyfetch.rb
+++ Formula/bunnyfetch.rb
@@ -2,7 +2,7 @@ class Bunnyfetch < Formula
   desc "🐰 macOS version of elenapan's bunnyfetch"
   homepage "https://github.com/strafe/bunnyfetch"
   url "https://github.com/strafe/bunnyfetch.git"
-  version "0.0.1"
+  version "0.0.2"
   license "MIT"
 
   def install
