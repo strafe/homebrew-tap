02566a35f1d891da71548f098f808166af5a97d7
strafe <strafe@users.noreply.github.com>
2021-03-02 22:36:06 +0000

    Add hjkl

---
+++ Formula/hjkl.rb
@@ -0,0 +1,11 @@
+class Hjkl < Formula
+  desc "DDR, but with Vi keys"
+  homepage "https://github.com/strafe/hjkl"
+  url "https://github.com/strafe/hjkl.git"
+  version "0.0.1"
+  license "MIT"
+
+  def install
+    bin.install "hjkl"
+  end
+end
