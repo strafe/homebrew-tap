0d0857962f3e96bccdf8641fe0afd248ad08fe0b
strafe <strafe@users.noreply.github.com>
2021-03-03 08:15:59 +0000

    Update hjkl to 0.0.2

--- Formula/hjkl.rb
+++ Formula/hjkl.rb
@@ -2,7 +2,7 @@ class Hjkl < Formula
   desc "DDR, but with Vi keys"
   homepage "https://github.com/strafe/hjkl"
   url "https://github.com/strafe/hjkl.git", branch: "main"
-  version "0.0.1"
+  version "0.0.2"
   license "MIT"
 
   def install
