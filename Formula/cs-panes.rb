5929ad66d11f369b521886d58aaa3f098b783ab1
strafe <strafe@users.noreply.github.com>
2021-03-07 15:18:03 +0000

    bump cs-panes

--- Formula/cs-panes.rb
+++ Formula/cs-panes.rb
@@ -2,7 +2,7 @@ class CsPanes < Formula
   desc 'Display your terminal colo(u)r palette using a series of panes'
   homepage 'https://github.com/strafe/color-scripts'
   url 'https://github.com/strafe/color-scripts.git'
-  version '1.0.0'
+  version '1.0.1'
 
   def install
     bin.install 'panes'
