981730faa77522c62cbdd5ad58e53cc1d5d1b614
strafe <strafe@users.noreply.github.com>
2021-03-08 10:28:09 +0000

    feat: add odourless cask

---
+++ Casks/odourless.rb
@@ -0,0 +1,14 @@
+cask "odourless" do
+  version "0.0.8"
+  sha256 :no_check
+
+  url "https://github.com/xiaozhuai/odourless/releases/download/#{version}/Odourless_#{version}.zip"
+
+  name "Odourless"
+  desc "A tool for preventing .DS_Store creation"
+  homepage "https://github.com/xiaozhuai/odourless"
+
+  auto_updates on
+
+  app "Odourless.app"
+end
