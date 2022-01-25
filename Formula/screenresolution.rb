b81b7517e271284ae1662e8766c837f19aa40cab
strafe <strafe@users.noreply.github.com>
2022-01-25 23:54:03 +0000

    chore: use hash of new archive

--- Formula/screenresolution.rb
+++ Formula/screenresolution.rb
@@ -2,7 +2,7 @@ class Screenresolution < Formula
   desc "Get, set, and list display resolution"
   homepage "https://github.com/ghostwriternr/screenresolution"
   url "https://github.com/ghostwriternr/screenresolution/archive/cc16458aa083e7bdcc61057a5607da5a67a6b679.tar.gz"
-  sha256 "d3761663eaf585b014391a30a77c9494a6404e78e8a4863383e12c59b0f539eb"
+  sha256 "0ec04aed82ff0a9f15ea98b42f980ee4c67510d9a8316b734a3ba60f687d6e02"
   license "GPL-2.0-only"
   head "https://github.com/ghostwriternr/screenresolution", branch: "add-max"
 
